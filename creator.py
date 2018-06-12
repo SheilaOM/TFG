2fa58b2d059cb8cba55c17da618ddf8729a90dd0
SheilaOM <sheila.oliva14@gmail.com>
2018-06-12 21:54:48 +0200

    Eliminado el uso de la sheet 'form' y añade los nombres de los campos a mano. Modificados procesos de graphics y lists para que funcione tras aliminar la sheet 'form'. Otros cambios menores para adaptarlo a esta modificación. Añadido proceso de ordenar alfabéticamente los participantes.

--- creator.py
+++ creator.py
@@ -20,27 +20,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from operator import itemgetter
 
+#SPREADSHEET_ID = '1tX2SheuK8BFyp_bPEaFt_rs4gjRr_eXPEBnNadVdCaI' # id of Google Spreadsheet
+SPREADSHEET_ID = '1cWBAVb_pUqJlmlaxsXmajPK3601ZxToZWv6qP3wRj3g'
+HEADER = ['date', 'name', 'position', 'affiliation', 'nationality', 'graduation', 'dietary', 'picture', 'topics', 'looking_for', 'homepage', 'twitter', 'presentation', 'programming', 'hobbies', 'tabs', 'looking', 'hiring']
 
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Yearbook Generator'
-
 IMAGE_SIZE = 512, 512 # Maximum participant image size
 LIMIT_DESCRIPTION = 200 # Maximum participant description size
-SPREADSHEET_ID = '1tX2SheuK8BFyp_bPEaFt_rs4gjRr_eXPEBnNadVdCaI' # id of Google Spreadsheet
 
-"""
-URL original: https://docs.google.com/spreadsheets/d/1NtocNeyy0B2nOnz-Sw84EMRzejJIXcPm1g8E5Wk36u4/edit
-Campos y datos (URL utilizada): https://docs.google.com/spreadsheets/d/1tX2SheuK8BFyp_bPEaFt_rs4gjRr_eXPEBnNadVdCaI/edit
-"""
 class Creator():
 
     def __init__(self):
-        self.values = []        # NamedTuple with data
         self.fields = []        # NamedTuple with fields and their info
-        self.data = []          # data
-        self.field_names = ""
-        self.err = open("errors.txt", "w")  # FIXME: should be a parameter; default STDERR
+        self.err = open("errors.txt", "w", encoding="utf-8")  # FIXME: should be a parameter; default STDERR
                                             # FIXME: should be the path, not the file descriptor!
 
         try:
@@ -74,7 +68,7 @@ class Creator():
         return credentials
 
 
-    def get_datos(self):
+    def spreadsheet_to_namedtuple(self):
         """
         Obtains personal data from the Google spreadsheet
         and returns a namedtuple
@@ -86,31 +80,8 @@ class Creator():
         service = discovery.build('sheets', 'v4', http=http,
                                   discoveryServiceUrl=discoveryUrl)
 
-        #rangeName = 'Form Responses 1!A2:ZZZ'          # sheet, and rows and columns
-        rangeName = 'MSR!A2:ZZZ'                     # sheet, and rows and columns   - FIXME! Belongs to configuration
-        result = service.spreadsheets().values().get(
-            spreadsheetId=SPREADSHEET_ID, range=rangeName).execute()
-        self.data_form = result.get('values', [])
-
-        if not self.data_form:
-            print('No data found.')
-        else:
-            self.data_form = sorted(self.data_form,key=lambda x: x[1].split(" ")[1])        #Orders participants by family name
-            Data = namedtuple("Data", self.field_names)
-            for row in self.data_form:
-                data = Data(*row)
-                self.values.append(data)
-
-    #
-    def DataIn(self):
-        credentials = c.get_credentials()
-        http = credentials.authorize(httplib2.Http())
-        discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-                        'version=v4')
-        service = discovery.build('sheets', 'v4', http=http,
-                                  discoveryServiceUrl=discoveryUrl)
-
-        rangeName = 'form!A2:Z'                                             # sheet, and rows and columns  - FIXME! Belongs to configuration
+        rangeName = 'A2:R'
+#        rangeName = 'A2:'+ string.ascii_uppercase[len(HEADER)-1] # sheet, and rows and columns
         result = service.spreadsheets().values().get(
             spreadsheetId=SPREADSHEET_ID, range=rangeName).execute()
         values = result.get('values', [])
@@ -118,14 +89,13 @@ class Creator():
         if not values:
             print('No data found.')
         else:
-            Fields = namedtuple("Fields", "name text show statistics list")
+            Fields = namedtuple("Fields", ' '.join(HEADER))
             for row in values:
-                fields = Fields(*row)
-                self.fields.append(fields)
-                self.field_names += row[0] + " "        # fields for Namedtuple with data
-
-        c.get_datos()
-
+                if row and len(row) == len(HEADER):  # in case of an empty row (deleted by hand from the spreadsheet)
+                    fields = Fields(*row)
+                    self.fields.append(fields)
+                else:
+                    self.err.write("- Error. Not enough values in row: " + str(row) + "\n")
 
     def make_chars_latex_friendly(self, row):
         """
@@ -172,6 +142,7 @@ class Creator():
                 else:
                     im = Image.open("images/img" + str(id) + "." + imgType)
                     im.thumbnail(IMAGE_SIZE, Image.ANTIALIAS)
+                    im = im.convert('RGB')
                     im.save("images/img" + str(id) + "." + imgType, imgType)
 
                 image = "images/img" + str(id) + "." + imgType
@@ -227,66 +198,75 @@ class Creator():
         return description
 
 
-    def generate_graphs (self):
+    def generate_graphs (self, fields):
         """
-        FIXME: include docstring
+        Generate a pie chart with the statistics of the answers
+        of the indicated fields
         """
-        df = pd.DataFrame(self.fields,columns=["name", "text", "show", "statistics", "list"])
-        stats = pd.crosstab(index=df["statistics"], columns="frecuencia")
-        fila = stats.loc[stats.index == "yes"]
+        n=1
+        df = pd.DataFrame(self.fields, columns=HEADER)
+        for fld in HEADER:
+            if fld in fields:
+                plt.figure();
+                tab = pd.crosstab(index=df[fld],columns="frecuencia")
+                plt.pie(tab, autopct="%1.1f%%", pctdistance=0.8, radius=1.2)
+                plt.legend(labels=tab.index,bbox_to_anchor=(0.9, 1), loc="upper left")
+                plt.title(fld, fontsize=15)
+                plt.savefig("graph" + str(n) + ".png", bbox_inches="tight")
+                n += 1
 
-        msg = ""
-        n = 1
-        if not fila.empty:
-            n_graph = int(fila["frecuencia"])
-            data = pd.DataFrame(self.data_form, columns=self.field_names.split(" ")[:-1])
-            for fld in self.fields:
-                if fld.statistics == "yes":
-                    plt.figure();
-                    tab = pd.crosstab(index=data[fld.name],columns="frecuencia")
-                    plt.pie(tab, autopct="%1.1f%%", pctdistance=0.8, radius=1.2)
-                    plt.legend(labels=tab.index,bbox_to_anchor=(0.9, 1), loc="upper left")
-                    if fld.text != "":
-                        plt.title(fld.text, fontsize=15)
-                    else:
-                        plt.title(fld.name, fontsize=15)
+        msg = (r"\newpage" + "\n" +
+               r"\begin{figure}" + "\n" +
+               r"\centering" + "\n")
 
-                    plt.savefig("graph" + str(n) + ".png", bbox_inches="tight")
-                    n += 1
+        for i in range(n-1):
+            #msg += r"\subfigure{\includegraphics[width=0.49\textwidth]{graph" + str(i+1) + "}}" + "\n"
+            msg += r"\subfigure{\includegraphics[height=6cm]{graph" + str(i+1) + "}}" + "\n"
 
-            msg += (r"\newpage" + "\n" +
-                    r"\begin{figure}" + "\n" +
-                    r"\centering" + "\n")
-
-            for i in range(n-1):
-                #msg += r"\subfigure{\includegraphics[width=0.49\textwidth]{graph" + str(i+1) + "}}" + "\n"
-                msg += r"\subfigure{\includegraphics[height=6cm]{graph" + str(i+1) + "}}" + "\n"
-
-            msg += r"\end{figure}" + "\n"
+        msg += r"\end{figure}" + "\n"
 
         return msg
 
 
-    def generate_list (self):
+    def generate_list (self, fields):
         """
-        Looks in the participant list (self.values) for who is looking for a
-        new position or searching for applicants
+        Only for fields with "Yes/No" answers.
 
-        Returns two lists (looking, searching)
+        Looks in the participant list (self.fields) for who has a "Yes" in then
+        indicated field.
+
+        Returns lists with the results.
         """
         msg = ""
-        for fld in self.fields:
-            if fld.list == "yes":       #Find the fields with "Yes" in the list column
+        for fld in HEADER:
+            if fld in fields:
                 msg += (r"\newpage" + "\n" +
-                        r"\color{color1}\uppercase{\textbf{" + fld.text + r"}}" + "\n" +
+                        r"\color{color1}\uppercase{\textbf{" + fld + r"}}" + "\n" +
                         r"\color{color2}" + "\n" +
                         r"\begin{multicols}{2}" + "\n" +
                         r"\begin{itemize}" + "\n")
-                for row in self.values:                  #through all the participants
-                    if getattr(row, fld.name) == "Yes":     #Participants with "Yes" in the chosen field
+                for row in self.fields:                  #through all the participants
+                    if getattr(row, fld) == "Yes":     #Participants with "Yes" in the chosen field
                         msg += r"\item " + row.name + "\n"
                 msg += r"\end{itemize}" + "\n" + r"\end{multicols}"
         return msg
+
+
+    def order_by_name(self):
+        """
+        Orders participants data (self.fields) by surname
+        """
+        surnames = [field.name.split()[-1] for field in self.fields]
+        surnames = list(set(surnames))
+        surnames.sort()
+
+        newList = []
+        for surname in surnames:
+            for field in self.fields:
+                if field.name.split()[-1] == surname:
+                    newList.append(field)
+
+        self.fields = newList
 
 
     def namedtuple_to_latex (self):
@@ -294,18 +274,18 @@ class Creator():
         Outputs the LaTeX data
         """
         id = 1
+        c.order_by_name()
         msg = r"\section*{Participants}" + "\n"
 
-        for row in self.values:
+        for row in self.fields:
             print (id)
             row = c.make_chars_latex_friendly(row)
             image = c.download_image(row, id)
             flag = c.get_flag(row, id)
-            description = c.cut_presentation(row.description)
+            description = c.cut_presentation(row.presentation)
 
             im = Image.open(image)
             width, height = im.size
-
 
             # Required data
             msg += (r"\noindent\begin{minipage}{0.3\textwidth}" + "\n" +
@@ -326,8 +306,8 @@ class Creator():
             if flag:
                 msg += r"\hspace{0.2cm}\includegraphics{" + flag + "}" + "\n"
 
-            if row.YearPhD:
-                msg += (r"\hspace{0.1cm}{\scriptsize (PhD " + row.YearPhD + ")}\n")
+            if row.graduation:
+                msg += (r"\hspace{0.1cm}{\scriptsize (PhD " + row.graduation + ")}\n")
 
             # Checks if it is a Twitter handle
             if len(row.twitter.split()) == 1 and row.twitter[0] == "@":
@@ -344,9 +324,10 @@ class Creator():
             if description:
                 msg += (r"{\footnotesize " + description + r"}\\" + "\n")
 
-            if row.HomePage:
-                msg += (r"{\scriptsize " + row.HomePage + r"}" + "\n")
+            if row.homepage:
+                msg += (r"{\scriptsize " + row.homepage + r"}" + "\n")
 
+            """
             # Optional data
             opcional = False
             for fld in self.fields:
@@ -358,23 +339,18 @@ class Creator():
                         msg += getattr(row, fld.name) + r" - "
             if opcional:
                 msg = msg[:len(msg)-3]
+            """
 
             msg += r"\end{minipage}" + "\n"
 
-            """
-            if id%4 == 0:          # 4 participants per page. After fourth, new page
-                msg += r"\newpage" + "\n"
-            else:
-                msg += r"\newline\newline\newline\newline" + "\n"
-            """
-            msg += r"\newline\newline\newline" + "\n"
+            msg += r"\newline\newline\newline\newline" + "\n"
             id += 1
 
         # Stats
-        msg += c.generate_graphs()
+        #msg += c.generate_graphs(['hiring', 'looking'])
 
         # List of people
-        msg += c.generate_list()
+        #msg += c.generate_list(['hiring', 'looking'])
 
         self.err.close()
         return msg
@@ -382,10 +358,11 @@ class Creator():
 
 if __name__ == "__main__":
     c = Creator()
-    c.DataIn()
+    c.spreadsheet_to_namedtuple()
     participants = c.namedtuple_to_latex()
     partic = open("participants.tex", "w", encoding="utf-8")
     partic.write(participants)
+    partic.close()
 
     try:
         introd = open("intro.tex", "r", encoding="utf-8")
@@ -393,6 +370,7 @@ if __name__ == "__main__":
         introd.close()
         gener = open("generated.tex", "w", encoding="utf-8")     # FIXME: change to with
         gener.write(text)
+        gener.close()
         try:
             os.system("xelatex generated.tex")
             print("PDF has been generated --> generated.pdf")
@@ -401,5 +379,3 @@ if __name__ == "__main__":
 
     except FileNotFoundError:
         print("Participants section created. Include it in your .tex")
-
-    gener.close()
